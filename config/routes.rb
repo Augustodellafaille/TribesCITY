fac19ddc4d22908542d9f010bfa2435dc1024f61
Augustodellafaille <augusto.dellafaille@gmail.com>
2015-12-31 16:13:12 +0100

    debugging navbar, building user edit and user show

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+
 
   get 'welcome/index'
   root 'welcome#index'
   resources :parties
 
-  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
-
+  devise_for :users, :controllers => { registrations: 'registrations'}
+  resources :users
   get 'parties/:id', to: 'parties#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
